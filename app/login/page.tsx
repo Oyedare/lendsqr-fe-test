e072c76cc614281ff6f9c98e1a583d11fa66f181
Oyedare <ooyedaretemiloluwa@gmail.com>
2023-12-05 09:56:22 +0100

    create nextjs + scss  starter file

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '@/styles/styledLogin.scss'
+const page = () => {
+  return (
+    <div className='login-container'>
+      
+    </div>
+  )
+}
+
+export default page
