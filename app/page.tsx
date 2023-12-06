4322d08d8d81e3fe8aec3ed9be769d4da93a1bdc
Oyedare <ooyedaretemiloluwa@gmail.com>
2023-12-06 11:58:13 +0100

    login feature complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import styles from '../styles/page.module.scss'
-
+import ProtectedRoute from '@/utils/checkAuth'
 export default function Home() {
   return (
-    <>
+    <ProtectedRoute>
       <h1>Homepage</h1>
-    </>
+    </ProtectedRoute>
   )
 }
