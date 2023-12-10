bb9a4819b6e11bba37ce7804f9823254dc467de0
Oyedare <ooyedaretemiloluwa@gmail.com>
2023-12-10 18:56:32 +0100

    auth validation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import ProtectedRoute from "@/utils/checkAuth";
+
 export default function Home() {
   return (
-    <></>
+    <ProtectedRoute>
+      <h1>Home Page</h1>
+    </ProtectedRoute>
   )
 }
