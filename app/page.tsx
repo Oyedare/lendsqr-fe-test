9b5e606a86ac1f5f2b846f7044ee438899fd0ece
Oyedare <ooyedaretemiloluwa@gmail.com>
2023-12-10 17:54:19 +0100

    fix: possible build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-import Image from 'next/image'
-import styles from '../styles/page.module.scss'
-import ProtectedRoute from '@/utils/checkAuth'
-import Sidebar from '@/components/sidebar'
-import Navbar from '@/components/navbar'
 export default function Home() {
   return (
     <></>
